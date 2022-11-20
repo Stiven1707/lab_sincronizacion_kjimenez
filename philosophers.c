c206213ed5f1c4b3734cbc877df98526dcceb551
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2022-11-19 19:02:39 -0500

    Documentacion y pre-implementacion prodcons

--- philosophers.c
+++ philosophers.c
@@ -4,46 +4,51 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-//Alias de funciones 
+// Alias de funciones
 #define down sem_wait
 
 #define up sem_post
 
-//Deficnicion del tipo semaphore
+/**
+ * @brief Deficnicion del tipo semaphore
+ */
 typedef sem_t semaphore;
-//TODO
-typedef enum{
+/**
+ * @brief diferentes estados que atraviezan los filosofos
+ */
+typedef enum
+{
     THINKING,
     HUNGRY,
     EATING
-}philosopher_state;
-//mutex
+} philosopher_state;
+// mutex
 semaphore mutex;
-//Arreglo de semaforos
-semaphore * s;
-//Arreglo de id de filosofos
-int * ids;
-//Arreglo de ID de hilos
-pthread_t * t;
-//Arreglo de estados de los filosofos
-philosopher_state * state;
+// Arreglo de semaforos
+semaphore *s;
+// Arreglo de id de filosofos
+int *ids;
+// Arreglo de ID de hilos
+pthread_t *t;
+// Arreglo de estados de los filosofos
+philosopher_state *state;
 
 /**
  * @brief Hilo de filosofo
- * 
+ *
  * @param arg Id del filosofo (int*)
  * @return NULL
  */
-void * philosopher(void * arg);
+void *philosopher(void *arg);
 /**
  * @brief Indica que el filosofo esta pensando
- * 
+ *
  * @param i Posicion del filosofo en la mesa
  */
 void think(int i);
 /**
  * @brief Trata de tomar los tenedores
- * 
+ *
  * @param i Posicion del filosofo en la mesa
  * @note El hilo se bloquea si alguno de sus vecinos esta usando los tenedores
  * @note Si ninguno de lso vecinos esta usando los tenedores, no se bloquea
@@ -51,73 +56,84 @@ void think(int i);
 void take_forks(int i);
 /**
  * @brief Indica que el filosofo esta comiendo
- * 
+ *
  * @param i Posicion del filosofo en la mesa
  */
 void eat(int i);
 /**
  * @brief Indica que el filosofo dejo de comer
- * 
+ *
  * @param i Posicion del filosofo en la mesa
  * @note El filosofp desbloquea a sus vecinos si se encuentra bloqueados
  */
 void put_forks(int i);
-
+/**
+ * @brief Realiza la trancicion entre hambriento
+ * y comiendo si se puede
+ *
+ * @param i Posicion del filosofo en la mesa
+ */
+void test(int i);
 
 int main(int argc, char *argv[])
 {
-    //Cantidad de filosofos
+    // Cantidad de filosofos
     int n;
-    //Contador para los for
+    // Contador para los for
     int i;
-    //Validar la cantidad de argumentos de linea de comandos
+    // Validar la cantidad de argumentos de linea de comandos
     n = 0;
-    if(argc != 2){
+    if (argc != 2)
+    {
         fprintf(stderr, "Debe especificar la cantidad de filosofos\n");
         exit(EXIT_FAILURE);
     }
-    //Obtener n de la lista de comandos
+    // Obtener n de la lista de comandos
     n = atoi(argv[1]);
     if (n <= 2)
     {
         fprintf(stderr, "Debe especificar la cantidad de filosofos mayor que 2\n");
         exit(EXIT_FAILURE);
     }
-    //POST: n > 2
-    //Reservar memoria para el arreglo de semaforos
+    // POST: n > 2
+    // Reservar memoria para el arreglo de semaforos
     s = (semaphore *)malloc(n * sizeof(semaphore));
-    //Reservar memoria para los identidicadores
+    // Reservar memoria para los identidicadores
     t = (pthread_t *)malloc(n * sizeof(pthread_t));
     ids = (int *)malloc(n * sizeof(int));
-    //Reservar memoria para los estados de los filosofos
+    // Reservar memoria para los estados de los filosofos
     state = (philosopher_state *)malloc(n * sizeof(philosopher_state));
-    //Inicializar los semaforos
-
-    //Inicializar el mutex en 1
+    // Inicializar los semaforos
+    // Inicializar el mutex en 1
     sem_init(&mutex, 0, 1);
-    //Inicializar los semaforos de los filosofos
+    // Inicializar los semaforos de los filosofos
     for (i = 0; i < n; i++)
     {
         sem_init(&s[i], 0, 0);
     }
-    
-    //Crear los hilos
+
+    // Crear los hilos
     for (i = 0; i < n; i++)
     {
-        ids[i] = i;//inicializamor el ID de los filosofos i
-        pthread_create(&t[i], NULL,philosopher,(void *)&ids[i]);
+        ids[i] = i; // inicializamor el ID de los filosofos i
+        pthread_create(&t[i], NULL, philosopher, (void *)&ids[i]);
     }
-    //Dar 10 s para que los hilos se ejecuten
-    sleep(10);
+    // Dar 2 s para que los hilos se ejecuten
+    sleep(2);
+    // Libero la memoria que asigne dinamicamente
+    free(s);
+    free(t);
+    free(ids);
+    free(state);
 
-    
     return EXIT_SUCCESS;
 }
-void * philosopher(void * arg){
+void *philosopher(void *arg)
+{
     int i;
-    //i = [obtener el valor *][obtener un apuntador de entero (int *)arg];
+    // i = [obtener el valor *][obtener un apuntador de entero (int *)arg];
     i = *(int *)arg;
-    printf("philosopher %d started\n",i);
+    printf("philosopher %d started\n", i);
     while (1)
     {
         think(i);
@@ -125,28 +141,84 @@ void * philosopher(void * arg){
         eat(i);
         put_forks(i);
     }
-    
-    printf("philosopher %d finished\n",i);
 
+    printf("philosopher %d finished\n", i);
 }
 
-void think(int i){
-    printf("philosopher %d is thinking\n",i);
-}
-
-void take_forks(int i){
-    printf("philosopher %d is hungry! taking the forks...\n",i);
-
-}
-
-void eat(int i){
-    printf("philosopher %d is eating\n",i);
+void take_forks(int i)
+{
+    printf("philosopher %d is hungry! taking the forks...\n", i);
     down(&mutex);
+    state[i] = HUNGRY;
+    test(i);
     up(&mutex);
-
+    down(&s[i]);
+}
+void think(int i)
+{
+    printf("philosopher %d is thinking\n", i);
 }
 
-void put_forks(int i){
-    printf("philosopher %d is full! realsing the forks...\n",i);
+void eat(int i)
+{
+    printf("philosopher %d is eating\n", i);
+    /* down(&mutex);
+    up(&mutex); */
+}
 
+void put_forks(int i)
+{
+    // TODO LEFT and RIGHT
+    size_t LEFT;
+    size_t RIGHT;
+    int n = sizeof(state) / sizeof(state[0]);
+    if (i == 0)
+    {
+        LEFT = n;
+        RIGHT = i + 1;
+    }
+    else if (i = n)
+    {
+        LEFT = i - 1;
+        RIGHT = 0;
+    }
+    else
+    {
+        LEFT = i - 1;
+        RIGHT = i + 1;
+    }
+    printf("philosopher %d is full! realsing the forks...\n", i);
+    down(&mutex);
+    state[i] = THINKING;
+    test(LEFT);
+    test(RIGHT);
+    up(&mutex);
+}
+
+void test(int i)
+{
+    // TODO LEFT and RIGHT
+    size_t LEFT;
+    size_t RIGHT;
+    int n = sizeof(state) / sizeof(state[0]);
+    if (i == 0)
+    {
+        LEFT = n;
+        RIGHT = i + 1;
+    }
+    else if (i = n)
+    {
+        LEFT = i - 1;
+        RIGHT = 0;
+    }
+    else
+    {
+        LEFT = i - 1;
+        RIGHT = i + 1;
+    }
+    if (state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING)
+    {
+        state[i] = EATING;
+        up(&s[i]);
+    }
 }
--- prodcons.c
+++ prodcons.c
@@ -1,8 +1,158 @@
 #include <pthread.h> //al compilar se requiere -ptheread
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+// Alias de funciones
+#define down sem_wait
+#define up sem_post
+
+/**
+ * @brief Deficnicion del tipo semaphore
+ */
+typedef sem_t semaphore;
+
+// mutex, garantiza la exclusion mutua
+semaphore mutex;
+// Cuantos elementos le falta por producir
+semaphore empty;
+// Cuantos elementos tiene producidos
+semaphore full;
+// ID de hilo producer
+pthread_t t_producer;
+// ID de hilo consumer
+pthread_t t_consumer;
+// Arreglo de id de productos
+int *idprods;
+
+/**
+ * @brief Hilo del productor
+ *
+ * @param arg
+ * @return void*
+ * @note El hilo se bloquea si ya produjo la cantida maxima de productos, "empty == 0"
+ * @note El hilo se bloquea si el consumidor esta sacando un producto
+ * @note Desbloquea al consumidor si se encuentra bloqueado
+ */
+void *producer(void *arg);
+/**
+ * @brief Crea un nuevo producto
+ *
+ * @return int Identificador de ese producto
+ */
+int produce_item();
+/**
+ * @brief Se encarga de tomar el producto y almacenarlo
+ * en un arreglo
+ *
+ * @param item Nuevo producto
+ */
+void insert_item(int item);
+/**
+ * @brief Hilo del consumidor
+ *
+ * @param arg
+ * @return void*
+ * @note El hilo se bloquea si no hay productos que consumir, "full == 0"
+ * @note El hilo se bloquea si el productor esta insertando un item
+ * @note Desbloquea al productor si este se encuentra bloqueado
+ */
+void *consumer(void *arg);
+/**
+ * @brief Remueve el ultimo producto ingresado
+ *
+ */
+void remove_item();
+/**
+ * @brief Muestra el item consumido
+ *
+ */
+void consume_item();
+
 int main(int argc, char *argv[])
 {
+    // Cantidad de elemtos maximos que se desean producir
+    int n;
+    n = 0;
+    // Validar la cantidad de argumentos de la linea de comandos
+    if (argc != 2)
+    {
+        fprintf(stderr, "Debe especificar la cantidad del buffer\n");
+        exit(EXIT_FAILURE);
+    }
+    // Obtener n de la lista de comandos
+    n = atoi(argv[1]);
+    // Validar que la cantidad de productos max sea mayor a cero
+    if (n <= 0)
+    {
+        fprintf(stderr, "Debe especificar la cantidad del buffer mayor que 0\n");
+        exit(EXIT_FAILURE);
+    }
+    // POST: n > 0
+    // Reservar memoria para los identidicadores de productos
+    idprods = (int *)malloc(n * sizeof(int));
+
+    // Inicializar los semaforos
+
+    // Inicializar el mutex en 1
+    sem_init(&mutex, 0, 1);
+    // Inicializar el empty en N
+    printf("n: %d\n",n);
+    sem_init(&empty, 0, n);
+    printf("empty: %d\n",sem_getvalue(&empty,&n));
+    // Inicializar el full en 0
+    sem_init(&full, 0, 0);
+
+    // Crear los hilos
+    pthread_create(&t_producer, NULL, producer, NULL);
+    // Dar 5 s para que los hilos se ejecuten
+    sleep(5);
+    // Libero la memoria que asigne dinamicamente
+    free(idprods);
     return EXIT_SUCCESS;
+}
+
+void *producer(void *arg)
+{
+    // TODO
+    printf("Produccion %d iniciada\n", full);
+    int item;
+    while (1)
+    {
+        //item = produce_item();
+        down(&empty);
+        down(&mutex);
+        //printf("producto %d\n",item);
+        //insert_item(item);
+        up(&mutex);
+        up(&full);
+    } 
+    printf("Produccion %d terminada\n", full);
+}
+
+int produce_item()
+{
+    int i = sem_getvalue(&full, &i);
+    return i;
+}
+
+void insert_item(int item)
+{
+    // TODO
+}
+
+void *consumer(void *arg)
+{
+    // TODO
+}
+
+void remove_item()
+{
+    // TODO
+}
+
+void consume_item()
+{
+    // TODO
 }
